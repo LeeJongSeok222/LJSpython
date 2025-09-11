e4349bf13ff86bb57aa6ee46429793ebc940db28
LJS <js-inline@naver.com>
2025-09-11 11:19:44 +0900

    2025.09.11_continue 문 추가

--- 10_breake_pass.py
+++ 10_breake_pass.py
@@ -1,11 +1,27 @@
 cup = 0
 running = True
 
-while running:
-    cup += 1
-    print(cup)
-    if cup == 10:
-        #running = False
-        break
+# while running:
+#     cup += 1
+#     print(cup)
+#     if cup == 10:
+#         #running = False
+#         break
+#
+# print('while 문 종료')
 
-print('while 문 종료')
+for i in range(1, 10): # 1~9까지 출력해줘.
+
+    if i == 3: # i는 3 (조건문)
+        continue #3(조건문)을 제외해줘
+
+    if i == 6: #i는 6(조건문)
+        continue #6(조건문)을 제외해줘
+
+    if i == 9: #i는 9(조건문)
+        continue #9(조건문)을 제외해줘
+    print(i) #출력해
+
+
+
+
